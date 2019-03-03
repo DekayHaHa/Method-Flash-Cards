87c4958f33efabf38359a2f7a3615d3b0b36ef13
Devin Kapla <devinkapla@gmail.com>
2019-03-03 13:04:27 -0700

    Updates

---
+++ precache-manifest.e2f60b56feeb43590d60c79b7d850715.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efd3986184c2ad6dc5f8",
+    "url": "/Method-Flash-Cards/static/css/main.2394d541.chunk.css"
+  },
+  {
+    "revision": "efd3986184c2ad6dc5f8",
+    "url": "/Method-Flash-Cards/static/js/main.efd39861.chunk.js"
+  },
+  {
+    "revision": "c1a7e0fceb3e7e6faeb9",
+    "url": "/Method-Flash-Cards/static/js/runtime~main.c1a7e0fc.js"
+  },
+  {
+    "revision": "7c07c4421856b5076fcb",
+    "url": "/Method-Flash-Cards/static/js/2.7c07c442.chunk.js"
+  },
+  {
+    "revision": "6466ddc83efca23260410df3568270e8",
+    "url": "/Method-Flash-Cards/index.html"
+  }
+];
