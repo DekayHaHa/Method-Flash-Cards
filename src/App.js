e68c97104692cd928a7ce15c33c165a5e76b1138
Devin Kapla <devinkapla@gmail.com>
2019-03-03 13:12:17 -0700

    Http to Https

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     }
   }
   componentDidMount = () => {
-    fetch('http://memoize-datasets.herokuapp.com/api/v1/dkData')
+    fetch('https://memoize-datasets.herokuapp.com/api/v1/dkData')
       .then(response => response.json())
       .then(questions => {
         this.setState({
